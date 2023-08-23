a78905ce8f21b53a3dccd596e1298c96e0982940
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-08-23 11:00:00 +0300

    2.2 done

--- src/readrides_analys.py
+++ src/readrides_analys.py
@@ -1,5 +1,4 @@
 import collections
-import heapq
 import pprint
 from typing import Any
 
@@ -33,35 +32,27 @@ def count_total_rides_for_each_route() -> dict[str, int]:
 
 
 def greatest_increase_in_ridership(
-    top_routes_count: int = 5, year_range: tuple[int, int] = (2001, 2011)
+    top_routes_count: int = 5,
+    year_range: tuple[int, int] = (2001, 2011),
+    rides: list[Raw] = rides,
 ) -> Any:
     """Return the five bus routes with the greatest increase in ridership."""
-    priority_queue = []
-    route_total_rides = {}
     counter = collections.Counter()
-    for route in {ride.route for ride in rides}:
-        for year in range(*year_range):
-            route_sum = sum(
-                ride.rides
-                for ride in rides
-                if ride.route == route
-                and int(ride.date.split("/")[-1]) == year
-            )
-            counter[route] += route_sum
-        if len(priority_queue) < 5:
-            heapq.heappush(priority_queue, route_total_rides[route])
-        else:
-            heapq.heappushpop(priority_queue, route_total_rides[route])
-    return priority_queue
+
+    for ride in rides:
+        if year_range[0] <= int(ride.date.rsplit("/")[-1]) <= year_range[1]:
+            counter[ride.route] += ride.rides
+
+    return counter.most_common(top_routes_count)
 
 
 if __name__ == "__main__":
-    # print(f"Number of bus routes: {count_bus_routs()}")
-    # print(
-    #     f"Number of people on route 22 on 2011-02-02: "
-    #     f"{count_people_on_date('22', '02/02/2011')}"
-    # )
-    # pprint.pprint(
-    #     f"Total rides for each route: {count_total_rides_for_each_route()}"
-    # )
+    print(f"Number of bus routes: {count_bus_routs()}")
+    print(
+        f"Number of people on route 22 on 2011-02-02: "
+        f"{count_people_on_date('22', '02/02/2011')}"
+    )
+    pprint.pprint(
+        f"Total rides for each route: {count_total_rides_for_each_route()}"
+    )
     pprint.pprint(greatest_increase_in_ridership())
