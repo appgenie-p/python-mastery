d41cb20c4b18f33472b3cab44e1a786f0f0fbe2a
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-08-25 16:43:21 +0300

    minor

--- src/e20_readrides.py
+++ src/e20_readrides.py
@@ -1,22 +1,33 @@
 import csv
 from typing import Any, Callable
+
 from e20_readrides_ds import RideData
 
 Raw = tuple[str, str, str, int]
 
 
-def read_rides(filename: str, save_method: Callable[..., Any]):
-    records: list = []
-    with open(filename, "r") as f:
-        rows = csv.reader(f)
-        _ = next(rows)  # Skip headers
+class RidesRides:
+    def __init__(self, save_method: Callable[..., Any]) -> None:
+        self.records: list = []
+        self.save_method = save_method
+
+    def read_rides(self, filepath: str):
+        with open(filepath, "r") as f:
+            rows = csv.reader(f)
+            self._skip_header(rows)
+            self._generate_record(rows)
+        return self.records
+
+    def _skip_header(self, rows):
+        _ = next(rows)
+
+    def _generate_record(self, rows):
         for row in rows:
             route = row[0]
             date = row[1]
             daytype = row[2]
             rides = int(row[3])
-            records.append(save_method(route, date, daytype, rides))
-    return records
+            self.records.append(self.save_method(route, date, daytype, rides))
 
 
 def read_rides_as_columns(filename):
