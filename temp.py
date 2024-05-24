4f3fbf3825de670a84a0dabe1133eda3e7710156
Gennady <gpolejaev@gmail.com>
2024-05-24 10:11:05 +0800

    e75

--- temp.py
+++ temp.py
@@ -1,23 +1,22 @@
-def map(func, values):
-    return [func(x) for x in values]
+class MyMeta(type):
+    def __new__(cls, name, bases, dct):
+        print(f"Creating class {name}")
+        # Вы можете изменить или добавить атрибуты класса здесь
+        dct["created_by"] = "MyMeta"
+        return super().__new__(cls, name, bases, dct)
 
 
-def reduce(func, values, initial=0):
-    result = initial
-    for n in values:
-        result = func(n, result)
-    return result
+class MyClass(metaclass=MyMeta):
+    def __init__(self, value):
+        self.value = value
+
+    def display(self):
+        print(f"Value: {self.value}")
 
 
-def sum(x, y):
-    return x + y
+# Создание экземпляра класса MyClass
+obj = MyClass(42)
+obj.display()
 
-
-def square(x):
-    return x * x
-
-
-nums = [1, 2, 3, 4]
-result = reduce(sum, map(square, nums))
-
-type()
+# Проверка дополнительного атрибута, добавленного метаклассом
+print(obj.created_by)
